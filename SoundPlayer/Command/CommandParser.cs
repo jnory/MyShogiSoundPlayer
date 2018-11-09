3ddd784a505ac796f9ad4f4427aff9a65d6bb2bc
Jun-ya Norimatsu <do-not-send-to-this-address@jnory.com>
2018-11-09 17:20:48 +0900

    不要なdiffを戻す

--- SoundPlayer/Command/CommandParser.cs
+++ SoundPlayer/Command/CommandParser.cs
@@ -4,12 +4,12 @@ namespace SoundPlayer.Command
 {
     public class CommandParser
     {
-        public static SoundPlayer.Command.Command Parse(string commandStr)
+        public static Command Parse(string commandStr)
         {
             var tokens = Tokenize(commandStr);
             if (tokens.Length == 1 && tokens[0] == "exit")
             {
-                return new SoundPlayer.Command.Command
+                return new Command
                 {
                     Type = CommandType.Exit,
                 };
@@ -34,14 +34,14 @@ namespace SoundPlayer.Command
             }
             if (tokens.Length == 3 && tokens[0] == "play")
             {
-                return new SoundPlayer.Command.Command
+                return new Command
                 {
                     Type = CommandType.Play,
                     Args = new []{tokens[1], tokens[2]},
                 };
             }
 
-            return new SoundPlayer.Command.Command
+            return new Command
             {
                 Type = CommandType.Unknown,
             };
